240f0636ffd793904bba40013e96408b1b7d23d2
Rafael Caple <103788808+rafaelcaple@users.noreply.github.com>
2026-02-16 17:09:56 -0300

    first commit

---
+++ src/main/java/com/rafaelcaple/video_game_manager/VideoGameManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelcaple.video_game_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGameManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelcaple/video_game_manager/VideoGameManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelcaple.video_game_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGameManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
