ad106d7c7757e75c17eb35203d7c3b573d65949a
Rafael Caple <103788808+rafaelcaple@users.noreply.github.com>
2026-02-17 16:25:51 -0300

    save from rawg by id

--- src/main/java/com/rafaelcaple/video_game_manager/controller/GameController.java
+++ src/main/java/com/rafaelcaple/video_game_manager/controller/GameController.java
@@ -1,13 +1,11 @@
 package com.rafaelcaple.video_game_manager.controller;
 
+import com.rafaelcaple.video_game_manager.entity.Game;
 import com.rafaelcaple.video_game_manager.rawg.RawgClient;
 import com.rafaelcaple.video_game_manager.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -20,5 +18,9 @@ public class GameController {
     @GetMapping("/search")
     public ResponseEntity<Map<String,Object>> searchGames (@RequestParam String query) {
         return ResponseEntity.ok(service.searchGames(query));
+    }
+    @PostMapping("/save/{rawgId}")
+    public ResponseEntity<Game> saveFromRawg (@PathVariable Integer rawgId) {
+        return ResponseEntity.ok(service.saveFromRawg(rawgId));
     }
 }
--- src/main/java/com/rafaelcaple/video_game_manager/service/GameService.java
+++ src/main/java/com/rafaelcaple/video_game_manager/service/GameService.java
@@ -1,26 +1,32 @@
 package com.rafaelcaple.video_game_manager.service;
 
 import com.rafaelcaple.video_game_manager.entity.Game;
+import com.rafaelcaple.video_game_manager.enums.GameEnums;
+import com.rafaelcaple.video_game_manager.rawg.RawgClient;
 import com.rafaelcaple.video_game_manager.repository.GameRepository;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class GameService {
     private final GameRepository repo;
+    private final RawgClient rawgClient;
 
-    public List<Game> findAll (){
-        return repo.findAll();
+    public Map<String,Object> searchGames (String query) {
+        return rawgClient.searchGames(query);
     }
 
-    public Game save (Game game) {
+    public Game saveFromRawg(Integer rawgId) {
+        Map<String,Object> data = rawgClient.getGameById(rawgId);
+        Game game = new Game();
+        game.setTitle((String) data.get("name"));
+        game.setRawgId(rawgId);
+        game.setStatus(GameEnums.GameStatus.BACKLOG);
         return repo.save(game);
     }
 
-    public void delete (Game game) {
-        repo.delete(game);
-    }
 }
