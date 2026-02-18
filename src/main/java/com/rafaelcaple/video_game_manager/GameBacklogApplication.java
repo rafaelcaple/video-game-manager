d74dfe5631e65f2846b3e4cf204572e470baf2f3
Rafael Caple <103788808+rafaelcaple@users.noreply.github.com>
2026-02-18 17:11:27 -0300

    changed name of the project

--- src/main/java/com/rafaelcaple/video_game_manager/GameBacklogApplication.java
+++ src/main/java/com/rafaelcaple/video_game_manager/GameBacklogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VideoGameManagerApplication {
+public class GameBacklogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VideoGameManagerApplication.class, args);
+		SpringApplication.run(GameBacklogApplication.class, args);
 	}
 
 }
