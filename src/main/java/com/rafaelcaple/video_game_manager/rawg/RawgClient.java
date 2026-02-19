8d7cbf75047f86fcffd0c040a2a1e8622bdcf818
Rafael Caple <103788808+rafaelcaple@users.noreply.github.com>
2026-02-19 12:34:24 -0300

    added search_precise filter to rawgclient
    testing if search_precise param is better for search

--- src/main/java/com/rafaelcaple/video_game_manager/rawg/RawgClient.java
+++ src/main/java/com/rafaelcaple/video_game_manager/rawg/RawgClient.java
@@ -29,6 +29,7 @@ public class RawgClient {
                         .queryParam("exclude_additions", true)
                         .queryParam("metacritic", "1,100")
                         .queryParam("ordering", "-relevance")
+                        .queryParam("search_precise", true)
                         .build())
                 .retrieve()
                 .bodyToMono(Map.class)
